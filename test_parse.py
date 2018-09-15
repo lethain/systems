c00f3598f2e01143b8a38840356576f01ae61a2f
Will Larson <lethain@gmail.com>
2018-09-15 09:30:08 -0700

    Add a test that does something real.

--- test_parse.py
+++ test_parse.py
@@ -2,15 +2,34 @@
 import unittest
 
 import systems
+import parse
+
+
+EXAMPLE_FULL = """
+[a] > b @ 25
+b > c @ 0.5
+c > d @ 0.5, leak
+d > [e] @ 1.0
+"""
 
 
 class TestParse(unittest.TestCase):
-    def test_a(self):
-        self.assertEqual(1, 1)
-
-    def test_b(self):
-        self.assertEqual(1, 2)        
-
+    def test_parse(self):
+        model = parse.parse(EXAMPLE_FULL)
+        stock_names = ['a', 'b', 'c', 'd', 'e']
+        values = {
+            'a': float('+inf'),
+            'b': 0,
+            'e': float('+inf'),
+        }
+        for stock_name in stock_names:
+            stock = model.get_stock(stock_name)
+            self.assertIsNotNone(stock)
+            self.assertEqual(stock_name, stock.name)
+            if stock_name in values:
+                value = values[stock_name]
+                self.assertEqual(value, stock.initial)
+        
 
 if __name__ == "__main__":
     unittest.main()
