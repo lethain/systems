cad65b0bc4057415b5f6727b34eaa16f08a32886
Will Larson <lethain@gmail.com>
2018-09-17 06:33:42 -0700

    Move stuff into systems directory for packaging

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+        long_description = fh.read()
+
+        setuptools.setup(
+                name="systems",
+                version="0.0.1",
+                author="Will Larson",
+                author_email="lethain@gmail.com",
+                description="Describe and run systems diagrams.",
+                long_description=long_description,
+                long_description_content_type="text/markdown",
+                url="https://github.com/lethain/systems",
+                packages=setuptools.find_packages(),
+                classifiers=[
+                            "Programming Language :: Python :: 3",
+                            "License :: OSI Approved :: MIT License",
+                            "Operating System :: OS Independent",
+                        ],
+            )
